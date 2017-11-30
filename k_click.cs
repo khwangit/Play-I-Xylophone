aaa5b286349d2fecdd8d43b265a2469213621aa6
khwangit <wang.k@husky.neu.edu>
2017-11-29 21:36:56 -0500

    Initial commit

---
+++ k_click.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class k_click : MonoBehaviour {
+
+	public Sprite up;
+	public Sprite down;
+	public float delay = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.gameObject.GetComponent<SpriteRenderer>().sprite == down){
+			delay -= Time.deltaTime;
+			if (delay <= 0) {
+				this.gameObject.GetComponent<SpriteRenderer>().sprite = up;
+				delay = 0.1f;
+			}
+		}
+	}
+
+	void OnMouseDown(){
+		this.gameObject.GetComponent<SpriteRenderer>().sprite = down;
+		this.gameObject.GetComponent<AudioSource>().Play();
+	}
+}
